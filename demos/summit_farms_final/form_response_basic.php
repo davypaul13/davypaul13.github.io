45dbcc65e3e3305ae3b8510eb61f2aec99e7273b
Davy Paul <davypaul13@gmail.com>
2025-05-31 10:54:12 -0700

    Move build artefacts to repo root, remove docs/, add demos

---
+++ demos/summit_farms_final/form_response_basic.php
@@ -0,0 +1,39 @@
+
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>contact form response</title>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div id="container">
+<h1>Thank You</h1>
+<h2>We'll Get Back To You Shortly</h2>
+<hr>
+<p>&nbsp;</p>
+
+<?php
+
+echo "<p class='colorRed'>This page was created by PHP on the server and sent back to your browser. </p>";
+
+//It will create a table and display one set of name value pairs per row
+	echo "<table border='1'>";
+	echo "<tr><th>Field Name</th><th>Value of field</th></tr>";
+	foreach($_POST as $key => $value)
+	{
+		echo '<tr class=colorRow>';
+		echo '<td>',$key,'</td>';
+		echo '<td>',$value,'</td>';
+		echo "</tr>";
+	}
+	echo "</table>";
+	echo "<p>&nbsp;</p>";
+
+?>
+
+</body>
+</html>
